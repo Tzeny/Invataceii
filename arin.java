79839e634ae0d78d1148ff0e6408b79537f6daf2
Andrei Tenescu <teny96@gmail.com>
2017-03-13 19:48:17 +0200

    Initial commit

---
+++ arin.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+class Arin {
+
+	// http://localhost:8080/RESTfulExample/json/product/get
+	public static void main(String[] args) {
+
+			int []a = new int[10];
+			a[11]=10;
+
+	}
+
+}
---
+++ test.py
@@ -0,0 +1,21 @@
+#!flask/bin/python
+from flask import Flask
+import json
+import random
+
+app = Flask(__name__)
+
+host = "0.0.0.0"
+port = "1337"
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/GetSensorValue')
+def readSensor():
+    data = {"value":random.randrange(200,10000)}
+    return json.dumps(data)
+
+if __name__ == '__main__':
+    app.run(host,port)
